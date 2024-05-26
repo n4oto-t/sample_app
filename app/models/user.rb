640b8d15d94f4e1f060799f09d3dfe2f8ba7aff3
Naoto Takata <133556559+n4oto-t@users.noreply.github.com>
2024-05-26 10:24:39 +0000

    9-3 remember meテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ attr_accessor :remember_token
 
   def authenticated?(remember_token)
     return false if remember_digest.nil?
-  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
