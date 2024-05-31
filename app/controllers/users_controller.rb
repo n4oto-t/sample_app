db00e920822e3b56056e4cbc498eb0698bfd9773
Naoto Takata <133556559+n4oto-t@users.noreply.github.com>
2024-05-31 12:50:50 +0000

    10-1 ユーザーの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,22 @@ class UsersController < ApplicationController
   else
   render 'new' , status: :unprocessable_entity
   end
+end
+
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit' , status: :unprocessable_entity
+    end
+  end
+
   private
     def user_params
     params.require(:user).permit(:name , :email ,:password ,:password_confirmation)
