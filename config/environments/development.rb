c9879b2154eb783251e4c196ff36173f64743f88
Naoto Takata <133556559+n4oto-t@users.noreply.github.com>
2024-06-03 12:15:30 +0000

    11-2 アカウント有効化のメール送信

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  host = '<hex string>.app.github.dev'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
